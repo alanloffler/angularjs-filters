046182ebde549bd15ffb897cb29f7769ee76f1da
Alan <alanloffler@icloud.com>
2014-07-17 16:39:53 -0300

    Added "camelcase" filter.
    Camel case has two options:
    1) “all”: camel case all the words of a text.
    2) “first”: camel case the first word of a text.

--- filters.js
+++ filters.js
@@ -1,3 +1,23 @@
 angular.module("alanloffler.filters", [])
 .filter("camelcase", function() {
+	return function(text, type) {
+		var type = type || "all";
+		var filtered;
+		
+		switch(type) {
+			case "all":
+				var words = text.split(" ");
+				for(var i = 0; i < words.length; i++) {
+					words[i] = words[i].substr(0, 1).toUpperCase() + words[i].slice(1);
+				};
+				filtered = words.join(" ");
+			break;
+			
+			case "first":
+				filtered = text.substr(0, 1).toUpperCase() + text.slice(1);
+			break;
+		};
+		
+		return filtered;
+	};
 });
